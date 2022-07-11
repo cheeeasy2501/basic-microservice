3c220592ae327da97c956aa18dd39b5874ce242a
Nikita Koida <nikita.koido@intexsoft.by>
2022-07-11 17:36:36 +0300

    first commit

---
+++ cmd/app/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Configuration
+	//cfg, err := config.NewConfig()
+	//if err != nil {
+	//	log.Fatalf("Config error: %s", err)
+	//}
+	//
+	//// Run
+	//app.Run(cfg)
+}
---
+++ internal/app/app.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	v1 "basic-microservice/internal/transport/v1"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm/logger"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Run(cfg *config.Config) {
+	l := logger.New(cfg.Log.Level)
+
+	// Repository
+	pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax))
+	if err != nil {
+		l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
+	}
+	defer pg.Close()
+
+	// Use case
+	translationUseCase := usecase.New(
+		repo.New(pg),
+		webapi.New(),
+	)
+
+	// RabbitMQ RPC Server
+	rmqRouter := amqprpc.NewRouter(translationUseCase)
+
+	rmqServer, err := server.New(cfg.RMQ.URL, cfg.RMQ.ServerExchange, rmqRouter, l)
+	if err != nil {
+		l.Fatal(fmt.Errorf("app - Run - rmqServer - server.New: %w", err))
+	}
+
+	// HTTP Server
+	handler := gin.New()
+	v1.NewRouter(handler, l, translationUseCase)
+	httpServer := httpserver.New(handler, httpserver.Port(cfg.HTTP.Port))
+
+	// Waiting signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		l.Info("app - Run - signal: " + s.String())
+	case err = <-httpServer.Notify():
+		l.Error(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
+	case err = <-rmqServer.Notify():
+		l.Error(fmt.Errorf("app - Run - rmqServer.Notify: %w", err))
+	}
+
+	// Shutdown
+	err = httpServer.Shutdown()
+	if err != nil {
+		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
+	}
+
+	err = rmqServer.Shutdown()
+	if err != nil {
+		l.Error(fmt.Errorf("app - Run - rmqServer.Shutdown: %w", err))
+	}
+}
